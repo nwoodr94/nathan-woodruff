21566583e0b9cbba92a730105e17d0e86c0a5d3a
Nathan Woodruff <nwoodr94@gmail.com>
2020-05-07 23:19:13 -0700

    content

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -19,10 +19,10 @@ const aboutJSX = (
       </p>
       <h2>Software Development</h2>
       <p>
-        I'm a software developer at Southern California Edison. SCE provides electricity to 14 million people in the Southern California region.
+        I'm a software developer at Southern California Edison in Los Angeles. SCE provides electricity to 14 million people in the Southern California region.
         <br />
         <br />
-        I like to traverse the full-stack throughout the course of a typical sprint. 
+        I like to traverse the whole stack throughout the course of a typical sprint. 
         <br />
         <br />
         That means I code both front- and back-end features and modules in JavaScript, integrate third party APIs, script in Python and SQL, deploy applications
@@ -33,19 +33,19 @@ const aboutJSX = (
       </p>
       <h2>Machine Preferences</h2>
       <p>
-        I'm developing on a Pixelbook Go, with the i5/8/128G configuration. I like the abstract-everything Chrome OS
-        philosophy, but when it's time to code, I launch the terminal and work directly in the provisioned Debian Linux
+        I'm developing on a Pixelbook Go, with the i5/8/128G configuration. I like the abstract-everything
+        philosophy of Chrome OS, but when it's time to code, I fire up the terminal and work directly in the provisioned Debian Linux
         container.
       </p>
       <h2>My Website</h2>
       <p>
-        This website is developed in React/Python and hosted on AWS. All requests to this domain are handled by Route 53, and passed through CloudFront before landing in an S3 bucket, where you are now.
+        This website is developed in React, and hosted on AWS. All requests to this domain are handled by Route 53, and passed through CloudFront before landing in an S3 bucket, where you are now.
         <br />
         <br />
         The contact form submits information to a Flask server running on an EC2 instance. The form data is parsed on the backend and loaded into a SQLite database.
         <br />
         <br />
-        I'm using Elastic Beanstalk to manage the server configuration, networking, and security essentials needed to host a web application.
+        I'm using Elastic Beanstalk to manage the infrastructure configuration, networking, and security essentials needed to host this application.
         <br />
         <br />
         I collect form submissions from this site by securely posting a credential to the endpoint with cURL, in effect calling a function that returns my contacts.
@@ -57,8 +57,13 @@ const aboutJSX = (
         <br />
         In the future I'd like to fund my own research in space propulsion. I want to
         start my own business, and explore the west in an old Toyota 4Runner.
+      </p>
+      <h2>My Programming Story</h2>
+      <p>
+        This is the Space Daemon, a multiple-stage sounding rocket launched and recovered in the Black Rock Desert, NV.
         <br />
         <br />
+        I first learned to code by programming a C script that collected data from an array of onboard sensors and instrumentation.
       </p>
       <div class="img-container">
         <img src={require("../../assets/space-daemon.png")} alt="nathan's rocket"/>
